ae242d233f686a13980a6288d6cce8ba100258ea
temptingreality <lukebadger777@gmail.com>
2018-11-10 11:25:37 -0600

    added arm teleop! whoooooooooooooooooooo!
    -Q

--- TeamCode/src/main/java/org/timecrafters/temptingreality/engines/revRovingRobotEngine.java
+++ TeamCode/src/main/java/org/timecrafters/temptingreality/engines/revRovingRobotEngine.java
@@ -1,18 +1,17 @@
 package org.timecrafters.temptingreality.engines;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.timecrafters.engine.Engine;
-import org.timecrafters.temptingreality.states.revDistancesensorTest;
+import org.timecrafters.temptingreality.states.revRovingRobot;
 
 /**
  * Created by t420-1 on 10/2/2018.
  */
 @TeleOp(name = "rev roving robot", group = "Testing")
-public class distanceTestEngine extends Engine {
+public class revRovingRobotEngine extends Engine {
     @Override
     public void setProcesses() {
-        addState(new revDistancesensorTest(this));
+        addState(new revRovingRobot(this));
     }
 }
--- TeamCode/src/main/java/org/timecrafters/temptingreality/states/revRovingRobot.java
+++ TeamCode/src/main/java/org/timecrafters/temptingreality/states/revRovingRobot.java
@@ -15,12 +15,14 @@ import org.timecrafters.engine.State;
  * Created by t420-1 on 7/11/2018.
  */
 
-public class revDistancesensorTest extends State {
+public class revRovingRobot extends State {
     private DcMotor motor1;
     private DcMotor motor2;
+    private DcMotor motor3;
+    private CRServo servoElbow;
     
 public boolean firstrun = true;
-    public revDistancesensorTest(Engine engine)
+    public revRovingRobot(Engine engine)
 
 
     {
@@ -33,13 +35,15 @@ public boolean firstrun = true;
         super.init();
       motor1 = engine.hardwareMap.dcMotor.get("leftDrive");
       motor2 = engine.hardwareMap.dcMotor.get("righDrive");
-
-
+      motor3 = engine.hardwareMap.dcMotor.get("collectionArm");
+      servoElbow = engine.hardwareMap.crservo.get("collectionElbow");
     }
 
     @Override
     public void exec() {
 motor1.setPower(engine.gamepad1.left_stick_y*-1);
 motor2.setPower(engine.gamepad1.right_stick_y);
+motor3.setPower(engine.gamepad2.left_stick_y);
+servoElbow.setPower(engine.gamepad2.right_stick_x);
     }
 }
