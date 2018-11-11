d7e1fc8d45acaf7d9cbcf64aff8982ae6edf9f78
RobotNRP <RobotNRP@gmail.com>
2018-11-11 14:36:26 -0600

    Gold and Silver detection with scan.

--- TeamCode/src/main/java/org/timecrafters/Nartaniel/Autonomous/Arcitecture/Arc1/DriveToDetect.java
+++ TeamCode/src/main/java/org/timecrafters/Nartaniel/Autonomous/Arcitecture/Arc1/DriveToDetect.java
@@ -1,12 +1,47 @@
 package org.timecrafters.Nartaniel.Autonomous.Arcitecture.Arc1;
 
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.timecrafters.Nartaniel.Autonomous.Arcitecture.ArchitectureControl;
 import org.timecrafters.engine.Engine;
 import org.timecrafters.engine.State;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
 public class DriveToDetect extends State {
     private boolean Complete = false;
     public ArchitectureControl Control;
+    VuforiaLocalizer vuforiaLocalizer;
+    VuforiaLocalizer.Parameters parameters;
+    VuforiaTrackables visionTargets;
+    VuforiaTrackable target;
+    VuforiaTrackableDefaultListener listener;
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    private List<VuforiaTrackable> AllTargets = new ArrayList<VuforiaTrackable>();
+    private boolean targetVisible;
+
+    final int CAMERA_FORWARD_DISPLACEMENT  = 110;   // eg: Camera is 110 mm in front of robot center
+    final int CAMERA_VERTICAL_DISPLACEMENT = 200;   // eg: Camera is 200 mm above ground
+    final int CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+
+    OpenGLMatrix lastKnownLocation = null;
+    OpenGLMatrix phoneLocation;
+
+    public static final String VUFORIA_KEY = "AcU+kbn/////AAAAGWDmHA7mS0gCoiMy9pA5e1AVyLZeqKejLOtP9c3COfi9g9m4Cs1XuVQVdqRFhyrFkNUynXwrhQyV65hPnPkGgRky9MjHlLLCWuqdpHzDLJonuOSBh5zVO11PleXH+2utK1lCnbBxvOM+/OrB9EAHUBrcB0ItRxjzFQOe8TXrjGGe1IyjC/Ljke3lZf/LVVinej3zjGNqwsNQoZ0+ahxYNPCJOdzRFkXjyMDXJVDQYMtVQcWKpbEM6dJ9jQ9f0UFIVXANJ7CC8ZDyrl2DQ8o4sOX981OktCKWW0d4PH0IwAw/c2nGgt1t2V/7PwTwysBYM1N+SjVpMNRg52u9gNl9os4ulF6AZw+U2LcVj4kqGZDi";
 
 
     public DriveToDetect(Engine engine, ArchitectureControl control) {
@@ -15,13 +50,29 @@ public class DriveToDetect extends State {
     }
 
     public void init() {
-
+        setupVuforia();
     }
 
     @Override
     public void exec() {
         if (Control.RunDriveToDetect) {
-            Complete = true;
+            targetVisible = false;
+            for (VuforiaTrackable trackable : AllTargets) {
+                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                    engine.telemetry.addData("Visible Target", trackable.getName());
+                    engine.telemetry.update();
+                    targetVisible = true;
+
+                    // getUpdatedRobotLocation() will return null if no new information is available since
+                    // the last time that call was made, or if the trackable is not currently visible.
+                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                    if (robotLocationTransform != null) {
+                        lastKnownLocation = robotLocationTransform;
+                    }
+                    break;
+                }
+            }
+
             if (Complete) {
                 engine.telemetry.addLine("Completed DriveToDetect");
                 engine.telemetry.update();
@@ -33,4 +84,39 @@ public class DriveToDetect extends State {
         }
     }
 
+
+    public void setupVuforia() {
+        int cameraMonitorViewId = engine.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", engine.hardwareMap.appContext.getPackageName());
+        parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+
+        parameters.cameraDirection = CAMERA_CHOICE;
+        vuforiaLocalizer = ClassFactory.getInstance().createVuforia(parameters);
+
+        visionTargets = vuforiaLocalizer.loadTrackablesFromAsset("RoverRuckus");
+        VuforiaTrackable blueRover = visionTargets.get(0);
+        blueRover.setName("Blue-Rover");
+        VuforiaTrackable redFootprint = visionTargets.get(1);
+        redFootprint.setName("Red-Footprint");
+        VuforiaTrackable frontCraters = visionTargets.get(2);
+        frontCraters.setName("Front-Craters");
+        VuforiaTrackable backSpace = visionTargets.get(3);
+        backSpace.setName("Back-Space");
+
+        AllTargets.addAll(visionTargets);
+
+        OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES,
+                        CAMERA_CHOICE == FRONT ? 90 : -90, 0, 0));
+
+        for (VuforiaTrackable trackable : AllTargets)
+        {
+            ((VuforiaTrackableDefaultListener)trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        }
+
+        visionTargets.activate();
+
+    }
 }
--- TeamCode/src/main/java/org/timecrafters/Nartaniel/Autonomous/Arcitecture/Arc1/MineralDetect.java
+++ TeamCode/src/main/java/org/timecrafters/Nartaniel/Autonomous/Arcitecture/Arc1/MineralDetect.java
@@ -1,27 +1,94 @@
 package org.timecrafters.Nartaniel.Autonomous.Arcitecture.Arc1;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.timecrafters.Nartaniel.Autonomous.Arcitecture.ArchitectureControl;
 import org.timecrafters.engine.Engine;
 import org.timecrafters.engine.State;
 
+import java.util.ArrayList;
+
 public class MineralDetect extends State {
     private boolean Complete = false;
     public ArchitectureControl Control;
+    private long CurrentTime;
+    private boolean ServoDirection;
+    private boolean FirstRun = true;
+    private double ArmWaveAmount = 0.25;
+    private long ArmWavePeriod = 50;
+    private ArrayList<DistanceSensor> DistanceSensors;
+    private Servo LazerArmServo;
+    public double BuildHeightMM;
+    private int NumberOfDati;
+    private long PreviousTriggerTime;
+    private double GoldDetectThreshold;
+    public int ScanNumber;
+    public boolean isGold;
+    private boolean ScanCommence;
+    private ArrayList<Double> HightValues;
+    private int DatiRequirement;
+    private double ServoCurrentPos;
+
+    
 
 
     public MineralDetect(Engine engine, ArchitectureControl control) {
         this.engine = engine;
         this.Control = control;
+        this.HightValues = new ArrayList<>();
+        this.DistanceSensors = new ArrayList<>();
     }
 
     public void init() {
-
+        DistanceSensors.add(engine.hardwareMap.get(DistanceSensor.class,"distance0"));
+        DistanceSensors.add(engine.hardwareMap.get(DistanceSensor.class,"distance1"));
+        DistanceSensors.add(engine.hardwareMap.get(DistanceSensor.class,"distance2"));
+        DistanceSensors.add(engine.hardwareMap.get(DistanceSensor.class,"distance3"));
+        LazerArmServo = engine.hardwareMap.servo.get("lazerArmServo");
+        HightValues.add(0.0);
+        HightValues.add(0.0);
+        HightValues.add(0.0);
+        HightValues.add(0.0);
+        HightValues.add(0.0);
     }
 
     @Override
     public void exec() {
         if (Control.RunMineralDetect) {
-            Complete = true;
+            CurrentTime = System.currentTimeMillis();
+            if (FirstRun) {
+                FirstRun = false;
+                PreviousTriggerTime = CurrentTime;
+                engine.telemetry.addLine("FirstRun");
+                engine.telemetry.update();
+                sleep(1000);
+            }
+
+
+            ScanCommence = true;
+
+            if (ScanCommence) {
+                for (int index = 0; index < (HightValues.size() - 2); index++) {
+                    if (HightValues.get(index) > HightValues.get(HightValues.size() - 1)) {
+                        HightValues.set(HightValues.size() - 1, HightValues.get(index));
+                    }
+
+                }
+
+                if (CurrentTime - PreviousTriggerTime >= ArmWavePeriod) {
+                    lazerWave(0.7);
+
+                    PreviousTriggerTime = CurrentTime;
+                }
+
+
+
+            }
+
+
             if (Complete) {
                 engine.telemetry.addLine("Completed MineralDetect");
                 engine.telemetry.update();
@@ -33,4 +100,23 @@ public class MineralDetect extends State {
         }
     }
 
+    private void lazerWave(double servoMiddleVal) {
+        ServoCurrentPos = LazerArmServo.getPosition();
+
+        if (ServoDirection && ServoCurrentPos <= servoMiddleVal + ArmWaveAmount) {
+            LazerArmServo.setPosition(ServoCurrentPos + 0.02);
+        } else if (!ServoDirection && ServoCurrentPos >= servoMiddleVal - ArmWaveAmount) {
+            LazerArmServo.setPosition(ServoCurrentPos - 0.02);
+        } else if (ServoDirection && ServoCurrentPos >= servoMiddleVal + ArmWaveAmount) {
+            ServoDirection = false;
+        } else if (!ServoDirection && ServoCurrentPos <= servoMiddleVal - ArmWaveAmount) {
+            ServoDirection = true;
+        }
+    }
+
+    @Override
+    public void telemetry() {
+        engine.telemetry.addData("Current Time", CurrentTime);
+        engine.telemetry.addData("PreviousTime", PreviousTriggerTime);
+    }
 }
