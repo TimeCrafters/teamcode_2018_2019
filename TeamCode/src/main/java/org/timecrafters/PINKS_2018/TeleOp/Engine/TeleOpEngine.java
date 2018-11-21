24f661c55a2a9c40a83f1f506c1da6db165c958a
RobotNRP <RobotNRP@gmail.com>
2018-11-21 17:01:27 -0600

    Gold and Silver detection with scan.

--- TeamCode/src/main/java/org/timecrafters/PINKS_2018/TeleOp/Engine/TeleOpEngine.java
+++ TeamCode/src/main/java/org/timecrafters/PINKS_2018/TeleOp/Engine/TeleOpEngine.java
@@ -1,10 +1,10 @@
-package org.timecrafters.Nartaniel.TeleOp;
+package org.timecrafters.PINKS_2018.TeleOp.Engine;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.timecrafters.PINKS_2018.TeleOp.States.TeleOpState;
 import org.timecrafters.engine.Engine;
-@Disabled
+
 @TeleOp (name = "TeleOp")
 
 public class TeleOpEngine extends Engine {
