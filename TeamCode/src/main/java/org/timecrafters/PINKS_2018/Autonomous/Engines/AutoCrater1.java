d7c442e4cd5bece8c80610889697b7a418d06da8
RobotNRP <RobotNRP@gmail.com>
2018-11-24 16:51:22 -0600

    Gold and Silver detection with scan.

--- TeamCode/src/main/java/org/timecrafters/PINKS_2018/Autonomous/Engines/AutoCrater1.java
+++ TeamCode/src/main/java/org/timecrafters/PINKS_2018/Autonomous/Engines/AutoCrater1.java
@@ -2,20 +2,20 @@ package org.timecrafters.PINKS_2018.Autonomous.Engines;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.timecrafters.PINKS_2018.Autonomous.States.Step06DropRobot;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step07PostDropUTurn;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step08DriveToDetect;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step09MineralDetect;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step10MineralKick;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step11TeamMarkerDrive;
+import org.timecrafters.PINKS_2018.Autonomous.States.Step12TeamMarkerPlace;
 import org.timecrafters.PINKS_2018.Autonomous.States.Step13OptionADriveToPark_fromTMP;
 import org.timecrafters.PINKS_2018.Autonomous.States.Step13OptionBDriveToPark_fromMK;
 import org.timecrafters.PINKS_2018.Autonomous.Support.ArchitectureControl;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step08DriveToDetect;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step06DropRobot;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step09MineralDetect;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step10MineralKick;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step07PostDropUTurn;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step11TeamMarkerDrive;
-import org.timecrafters.PINKS_2018.Autonomous.States.Step12TeamMarkerPlace;
 import org.timecrafters.engine.Engine;
 
-@Autonomous (name = "Autonomous: Depot")
-public class AutoDepot1 extends Engine {
+@Autonomous (name = "Autonomous: Crater")
+public class AutoCrater1 extends Engine {
 
     @Override
     public void setProcesses() {
@@ -28,7 +28,7 @@ public class AutoDepot1 extends Engine {
         addState(new Step08DriveToDetect(this, control));
         addState(new Step09MineralDetect(this, control));
         addState(new Step10MineralKick(this, control));
-        addState(new Step11TeamMarkerDrive(this, control, true));
+        addState(new Step11TeamMarkerDrive(this, control, false));
         addState(new Step12TeamMarkerPlace(this, control));
         addState(new Step13OptionADriveToPark_fromTMP(this, control));
         addState(new Step13OptionBDriveToPark_fromMK( this, control));
