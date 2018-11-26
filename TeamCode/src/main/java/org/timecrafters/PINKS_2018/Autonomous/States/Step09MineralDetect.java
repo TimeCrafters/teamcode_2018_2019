dc3fa92616b5075f36a8056e1bcb08844448dd9c
RobotNRP <RobotNRP@gmail.com>
2018-11-25 20:24:26 -0600

    Gold and Silver detection with scan.

--- TeamCode/src/main/java/org/timecrafters/PINKS_2018/Autonomous/States/Step09MineralDetect.java
+++ TeamCode/src/main/java/org/timecrafters/PINKS_2018/Autonomous/States/Step09MineralDetect.java
@@ -1,5 +1,7 @@
 package org.timecrafters.PINKS_2018.Autonomous.States;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -23,22 +25,22 @@ public class Step09MineralDetect extends State {
     private double ArmWaveAmount = 0.25;
     private long ArmWavePeriod = 50;
     private ArrayList<DistanceSensor> DistanceSensors;
-    private double SensorGround0;
-    private double SensorGround1;
-    private double SensorGround2;
-    private double SensorGround3;
+
     private Servo LazerArmServo;
-    public double BuildHeightMM;
     private int NumberOfDati;
     private double GoldDetectThreshold;
+    private double ObjectDetectThreshold;
     public int ScanNumber;
     public boolean isGold;
     private boolean ScanCommence;
+
     private ArrayList<Double> HightValues;
     private ArrayList<Double> Cal0;
     private ArrayList<Double> Cal1;
     private ArrayList<Double> Cal2;
     private ArrayList<Double> Cal3;
+    private ArrayList<Double> GroundValues;
+
     private int DatiRequirement;
     private double ServoCurrentPos;
     private boolean DriveOnly;
@@ -52,7 +54,6 @@ public class Step09MineralDetect extends State {
     private double whealCircumference = 4;
     private boolean Calibrating;
 
-    
 
 
     public Step09MineralDetect(Engine engine, ArchitectureControl control) {
@@ -64,16 +65,16 @@ public class Step09MineralDetect extends State {
         this.Cal1 = new ArrayList<>();
         this.Cal2 = new ArrayList<>();
         this.Cal3 = new ArrayList<>();
+        this.GroundValues = new ArrayList<>();
     }
 
     public void init() {
         DriveOnly = Control.AppReader.get("RunMineralDetect").variable("OnlyDrive");
         distanceIN = Control.AppReader.get("RunMineralDetect").variable("distanceIN");
         CalibrationTime = Control.AppReader.get("RunMineralDetect").variable("CalTime");
-
-
-        LeftDrive = Control.PinksHardwareConfig.pLeftMotor;
-        RightDrive = Control.PinksHardwareConfig.pRightMotor;
+        GoldDetectThreshold = Control.AppReader.get("RunMineralDetect").variable("GoldDT");
+        ObjectDetectThreshold = Control.AppReader.get("RunMineralDetect").variable("ObjectDT");
+        DatiRequirement = Control.AppReader.get("RunMineralDetect").variable("Dati");
 
         if (DriveOnly) {
             Power = Control.AppReader.get("RunMineralDetect").variable("DOPower");
@@ -81,19 +82,20 @@ public class Step09MineralDetect extends State {
             Power = Control.AppReader.get("RunMineralDetect").variable("ScanPower");
         }
 
-        DistanceConverter();
+        LeftDrive = Control.PinksHardwareConfig.pLeftMotor;
+        RightDrive = Control.PinksHardwareConfig.pRightMotor;
+        LazerArmServo = Control.PinksHardwareConfig.pLaserArmServo;
 
         DistanceSensors.add(Control.PinksHardwareConfig.pMineralDetect0);
         DistanceSensors.add(Control.PinksHardwareConfig.pMineralDetect1);
         DistanceSensors.add(Control.PinksHardwareConfig.pMineralDetect2);
         DistanceSensors.add(Control.PinksHardwareConfig.pMineralDetect3);
 
-        LazerArmServo = Control.PinksHardwareConfig.pLaserArmServo;
-        HightValues.add(0.0);
-        HightValues.add(0.0);
-        HightValues.add(0.0);
-        HightValues.add(0.0);
-        HightValues.add(0.0);
+        DistanceConverter();
+
+        for (int i = 0; i < DistanceSensors.size() + 1; i++) {
+            HightValues.add(0.0);
+        }
 
         FirstRun = true;
         Calibrating = true;
@@ -115,7 +117,6 @@ public class Step09MineralDetect extends State {
                 LeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 RightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 StartTime = CurrentTime;
-                PreviousTriggerTime = CurrentTime;
                 RightDrive.setPower(Power);
                 LeftDrive.setPower(Power);
             }
@@ -129,6 +130,10 @@ public class Step09MineralDetect extends State {
 
             if (!DriveOnly) {
 
+// -------------------------------------------------------------------------------------------------
+// The Calibration Records several values, then averages for each sensor individually, so that we know
+//the distance from ground, and thus the height of the object;
+
                 if (Calibrating) {
                     pause(true);
                     if (CurrentTime - StartTime < CalibrationTime) {
@@ -137,44 +142,115 @@ public class Step09MineralDetect extends State {
                         Cal2.add(DistanceSensors.get(2).getDistance(DistanceUnit.MM));
                         Cal3.add(DistanceSensors.get(3).getDistance(DistanceUnit.MM));
                     } else {
-                        
-                    }
 
-
-                }
-
-
-                if (ScanCommence) {
-                    //Stops drive temporarily
-                    pause(true);
-
-                    //Remember the Highest value while scanning
-                    for (int index = 0; index < (HightValues.size() - 2); index++) {
-                        if (HightValues.get(index) > HightValues.get(HightValues.size() - 1)) {
-                            HightValues.set(HightValues.size() - 1, HightValues.get(index));
+                        for (int i = 0; i < DistanceSensors.size(); i++) {
+                            GroundValues.add(0.0);
                         }
 
+                        for (int i = 0; i < Cal0.size(); i++) {
+                            GroundValues.set(0, GroundValues.get(0) + Cal0.get(i));
+                        }
+                        GroundValues.set(0, GroundValues.get(0) / Cal0.size());
+
+                        for (int i = 0; i < Cal1.size(); i++) {
+                            GroundValues.set(1, GroundValues.get(1) + Cal1.get(i));
+                        }
+                        GroundValues.set(1, GroundValues.get(1) / Cal1.size());
+
+                        for (int i = 0; i < Cal2.size(); i++) {
+                            GroundValues.set(2, GroundValues.get(2) + Cal2.get(i));
+                        }
+                        GroundValues.set(2, GroundValues.get(2) / Cal2.size());
+
+                        for (int i = 0; i < Cal3.size(); i++) {
+                            GroundValues.set(3, GroundValues.get(3) + Cal3.get(3));
+                        }
+                        GroundValues.set(3, GroundValues.get(3) / Cal3.size());
+
+                        Calibrating = false;
+                    }
+                }
+
+// -------------------------------------------------------------------------------------------------
+
+                if (!Calibrating) {
+
+                    //records heights of objects using sensor height from ground
+                    for (int i = 0; i < HightValues.size() - 1; i++) {
+                        HightValues.set(i, GroundValues.get(i) - DistanceSensors.get(i).getDistance(DistanceUnit.MM));
                     }
 
-                    //Waves Laser Arm
-                    if (CurrentTime - PreviousTriggerTime >= ArmWavePeriod) {
-                        lazerWave(0.7);
-                        PreviousTriggerTime = CurrentTime;
+
+                    //Checks the 4 height values to see if an object is present and act accordingly
+                    if (!ScanCommence) {
+                        for (int index = 0; index < (HightValues.size() - 1); index++) {
+                            if (HightValues.get(index) >= ObjectDetectThreshold) {
+                                ScanCommence = true;
+                                NumberOfDati = 0;
+                                ServoDirection = true;
+                                PreviousTriggerTime = CurrentTime;
+                            }
+                        }
                     }
 
 
+                    if (ScanCommence) {
+
+                        //Remember the Highest value while scanning
+                        for (int index = 0; index < (HightValues.size() - 1); index++) {
+                            if (HightValues.get(index) > HightValues.get(HightValues.size() - 1)) {
+                                HightValues.set(HightValues.size() - 1, HightValues.get(index));
+                            }
+                        }
+
+                        //Counts the number of unique data values detected in scan
+                        int i = 0;
+                        for (DistanceSensor sensor : DistanceSensors) {
+                            for (double PrevDataValue : HightValues) {
+                                if (GroundValues.get(i) - sensor.getDistance(DistanceUnit.MM) != PrevDataValue) {
+                                    NumberOfDati++;
+                                }
+                            }
+                            i++;
+                        }
+
+                        //if it reaches the end of the object before sufficient Dati is acquired, it
+                        //pauses the drive and waves the scan arm over the object for More Dati
+                        if (HightValues.get(0) < ObjectDetectThreshold && HightValues.get(1) < ObjectDetectThreshold && HightValues.get(2) < ObjectDetectThreshold && HightValues.get(3) < ObjectDetectThreshold) {
+                            //Temporarily Stops Drive
+                            pause(true);
+
+                            //Waves Laser Arm
+                            if (CurrentTime - PreviousTriggerTime >= ArmWavePeriod) {
+                                lazerWave(0.7);
+                                PreviousTriggerTime = CurrentTime;
+                            }
+                        }
+
+                        //When Enough Dati are gathered, determine if gold and act accordingly.
+                        if (NumberOfDati >= DatiRequirement) {
+                            ScanCommence = false;
+                            if (HightValues.get(HightValues.size() - 1) <= GoldDetectThreshold) {
+                                isGold = true;
+                                Complete = true;
+                            } else {
+                                ScanNumber++;
+                                resetHights();
+                                pause(false);
+                            }
+                        }
+                    }
                 }
             }
-
 
             if (Complete) {
                 engine.telemetry.addLine("Completed Step09MineralDetect");
                 engine.telemetry.update();
                 sleep(1000);
                 setFinished(true);
+            } else {
+                setFinished(true);
             }
-        } else {
-            setFinished(true);
         }
     }
 
@@ -196,7 +272,11 @@ public class Step09MineralDetect extends State {
         distanceTicks = (int) ((distanceIN * 288) / (whealCircumference * Math.PI));
     }
 
-
+    public void resetHights() {
+        for (int i = 0; i < HightValues.size(); i++) {
+            HightValues.set(i, 0.0);
+        }
+    }
 
     public void pause(boolean pause) {
         if (pause) {
@@ -211,5 +291,22 @@ public class Step09MineralDetect extends State {
     @Override
     public void telemetry() {
         engine.telemetry.addLine("Running Mineral Detect");
+
+        engine.telemetry.addData("Calibrating", Calibrating);
+
+        engine.telemetry.addData("Scan #", ScanNumber);
+
+        engine.telemetry.addData("Arm Position", LazerArmServo.getPosition());
+
+        for (double sensor: HightValues) {
+            engine.telemetry.addData("Hight Value", sensor);
+        }
+
+        engine.telemetry.addData("Max Hight", HightValues.get(HightValues.size() - 1));
+        engine.telemetry.addData("Dati in one Scan", NumberOfDati);
+        if (isGold) {engine.telemetry.addLine("IS GOLD!!!");}
+        if (ScanCommence) {engine.telemetry.addLine("Scanning");}
+        if (!ScanCommence) {engine.telemetry.addLine("Not Not Scanning");}
+
     }
 }
